739a82a0d0619cc3f731969684e11582c1e56991
André Dietrich <andre.dietrich@ovgu.de>
2018-08-27 11:51:54 +0200

    initial commit

---
+++ spec/liascript-preview-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LiascriptPreviewView from '../lib/liascript-preview-view';
+
+describe('LiascriptPreviewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
